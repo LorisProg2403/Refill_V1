216d6b9935ea5d8dbcd3ac147dfd35d908c3efaf
LorisProg2403 <lorisprog@gmail.com>
2025-03-04 03:47:02 +0100

    Initial Commit

---
+++ src/main/java/net/loris/refillv1/Refill_V1.java
@@ -0,0 +1,17 @@
+package net.loris.refillv1;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Refill_V1 implements ModInitializer {
+	public static final String MOD_ID = "refill_v1";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/loris/refillv1/Refill_V1Client.java
@@ -0,0 +1,10 @@
+package net.loris.refillv1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Refill_V1Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
